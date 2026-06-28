953543e4b405d037eb15a370b0b1255a4c52d6a8
papa <papa>
2026-06-28 14:43:07 +0900

    Disable SS_SHOULD_BE_STATIC like other card-model classes

--- src/main/java/jenkins/plugins/office365connector/model/adaptivecard/MentionEntity.java
+++ src/main/java/jenkins/plugins/office365connector/model/adaptivecard/MentionEntity.java
@@ -1,9 +1,11 @@
 package jenkins.plugins.office365connector.model.adaptivecard;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import jenkins.plugins.office365connector.model.Mention;
 
 public class MentionEntity {
 
+    @SuppressFBWarnings(value = "SS_SHOULD_BE_STATIC")
     private final String type = "mention";
     private final String text;
     private final Mentioned mentioned;
